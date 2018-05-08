6dfe29bdaf3664441622823af41e45e0cfed42e5
wino6687 <wino6687@colorado.edu>
2018-05-08 10:43:17 -0600

    Add files via upload

--- flowMod.py
+++ flowMod.py
@@ -1,7 +1,16 @@
 import datetime
+
 import subprocess
 import shlex
 from subprocess import call
+
+import numpy as np
+from netCDF4 import Dataset
+from skimage.measure import block_reduce
+
+from mapboxgl.utils import *
+from mapboxgl.viz import *
+
 import pandas as pd
 import ease2conv as e2
 from nco import Nco
@@ -12,6 +21,7 @@ nco = Nco()
 
 
 def scrape(dates, path, path_data):
+    '''WGET web scraper. supposed to be temporary'''
     os.chdir(path_data)
     # store the path of the url before the data (the date is updated on the fly)
     file_pre = 'https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0630.001/'
@@ -23,7 +33,7 @@ def scrape(dates, path, path_data):
     for date in tqdm(dates):
         stri = date.strftime('%Y.%m.%d')
         # convert datetimeindex to date time
-        temp = date # pd.datetime.strptime(date, '%Y.%m.%d')
+        temp = date #
         # Store the year of the current date, convert to day of year
         year = temp.year
         temp = str(temp.timetuple().tm_yday)
@@ -52,6 +62,8 @@ def scrape(dates, path, path_data):
 
 
 def get_xy(ll_ul, ll_lr):
+    '''Use NSIDC scripts to convert user inputted
+    lat/lon into Ease grid 2.0 coordinates'''
     N6 = e2.Ease2Transform("EASE2_N6.25km")
     N3 = e2.Ease2Transform("EASE2_N3.125km")
     # get x,y for 6.25
@@ -68,7 +80,11 @@ def get_xy(ll_ul, ll_lr):
     list_3 = [x3ul, y3ul, x3lr, y3lr]
     return list_3
 
+## take a path to .nc dataset to subset and the coordinates
 def subset(list6, path):
+    '''pass geo-coord list and directory path
+    script will get the files from wget directory
+    and subset them geographically'''
     path19, path37, wget = file_setup(path)
     os.chdir(wget)
     # Make a list of the files to concatenate together for 19H
@@ -97,8 +113,11 @@ def subset(list6, path):
         nco.ncks(input=infile, output=outfile, options=opt)
         os.remove(infile)
 
-
+## use list of paths as parameter to concatenate all paths in list
+## ['/folder/file1.nc','/folder/file2.nc']
 def concatenate(path, outfile_19, outfile_37, final=False):
+    '''Function to concatenate files in the subsetted data
+    folders.'''
     os.chdir(path + '/data' + '/Subsetted_19H')
     if final: # final we want to take diff file names
         list19 = glob.glob('19H*nc')
@@ -130,6 +149,8 @@ def concatenate(path, outfile_19, outfile_37, final=False):
             os.remove(f)
     else:
         print("No 37Ghz Files to Concatenate")
+    return outfile_19, outfile_37
+
 
 def file_setup(path):
     os.chdir(path)
@@ -148,21 +169,19 @@ def file_setup(path):
 
 def scrape_all(start, end, list3, path=None):
     '''Function to ensure we subset
-     and concatenate every year!'''
-    if path is None:
+     and concatenate every year!
+     Implements the whole workflow!'''
+    if path is None:   # if no directory path provided, find one
         path = os.getcwd()
     os.chdir(path)
-    small = False
     outfile19 = 'all_days_19H.nc'
     outfile37 = 'all_days_37H.nc'
     path19, path37, wget = file_setup(path)
     dates = pd.date_range(start, end)
     if len(dates) <= 133:
-        small = True
         scrape(dates, path, wget)
         subset(list3, path)
-        concatenate(path, outfile19, outfile37)
-        return
+        return concatenate(path, outfile19, outfile37)
     else:
         comp_list = [dates[x:x + 100] for x in range(0, len(dates), 100)]
         temp = 1
@@ -173,25 +192,49 @@ def scrape_all(start, end, list3, path=None):
             scrape(subList, path, wget)
             subset(list3, path)
             concatenate(path, tempfile19, tempfile37)
-        concatenate(path, outfile19, outfile37, final=True)
+        return concatenate(path, outfile19, outfile37, final=True)
 
 
 
+def plot_a_day(file1, file2, path, token):
+    '''read tb,x,y data from final files,
+    with the purpose of plotting.'''
+    os.chdir(path + '/data/Subsetted_19H')
+    fid_19H = Dataset(file1, "r", format="NETCDF4")
+    os.chdir(path + '/data/Subsetted_37H')
+    fid_37H = Dataset(file2, "r", format="NETCDF4")
+    x = fid_19H.variables['x'][:]
+    y = fid_37H.variables['y'][:]
+    tb_19H = fid_19H.variables['TB'][:]
+    tb_37H = fid_37H.variables['TB'][:]
+    tb_37H = block_reduce(tb_37H, block_size = (1,2,2), func = np.mean)
+    tb= tb_19H  - tb_37H
+    lats = np.zeros((len(y), len(x)), dtype=np.float64)
+    lons = np.zeros((len(y), len(x)), dtype=np.float64)
+    grid = e2.Ease2Transform(gridname=fid_19H.variables["crs"].long_name)
+    for i, xi in enumerate(x):
+        for j, yj in enumerate(y):
+            row, col = grid.map_to_grid(xi, yj)
+            lat, lon = grid.grid_to_geographic(row, col)
+            lats[j, i] = lat
+            lons[j, i] = lon
+    one_day = tb[0,:,:]
+    df = pd.DataFrame(columns = ['lat', 'lon', 'swe'])
+    for i in range(len(one_day[:,1])):
+        for j in range(1,:):
+            df = df.append({'lat': lats[i][j], 'lon': lons[i][j], 'swe':one_day[i][j]}, ignore_index = True)
+    df_to_geojson(df, filename = 'swe_1day.geojson',properties = ['swe'],lat = 'lat', lon = 'lon')
+    measure = 'swe'
+    color_breaks = [round(df[measure].quantile(q=x*0.1), 2) for x in range(1,9)]
+    color_stops = create_color_stops(color_breaks, colors='YlGnBu')
 
+    # Create the viz from the dataframe
+    viz = CircleViz('swe_1day.geojson',
+                    access_token=token,
+                    color_property = "swe",
+                    color_stops = color_stops,
+                    center = (-154, 67),
+                    zoom = 3,
+                    below_layer = 'waterway-label')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    viz.show()
