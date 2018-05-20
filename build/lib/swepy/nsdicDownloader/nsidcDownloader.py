a14d2f7d6b6f6c13fece076b8dde55535cf0e5c2
WILLIAM Norris <williamnorris@WILLIAMs-MacBook-Pro.local>
2018-05-20 14:03:27 -0600

    adding everything for pip installation

---
+++ build/lib/swepy/nsdicDownloader/nsidcDownloader.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+
+from tqdm import tqdm
+from datetime import datetime
+from functools import partial
+from string import Formatter
+
+import pandas as pd
+
+try:
+    if get_ipython().config['IPKernelApp']['parent_appname'] == 'ipython-notebook':
+        from tqdm import tqdm_notebook as tqdm
+except:
+    pass
+
+class nsidcDownloader():
+
+    url_template = "{protocol}://{server}/{datapool}/{dataset}.{version}/{date:%Y.%m.%d}" \
+                    "/{dataset}-{projection}_{grid}{resolution}-{platform}_{sensor}" \
+                    "-{date:%Y%j}-{channel}-{pass}-{algorithm}-{input}-{dataversion}.nc"
+
+    defaults = {
+        "protocol": "https",
+        "server": "n5eil01u.ecs.nsidc.org",
+        "datapool": "MEASURES",
+        "dataset": "NSIDC-0630",
+        "version": "001",
+        "projection": "EASE2",
+        "grid": "N",
+        "pass": "M",
+        "algorithm": "SIR",
+        "input": "CSU",
+        "dataversion": "v1.2"
+    }
+
+    def __init__(self, username=None, password=None, folder=".", **kwargs):
+        '''
+        Snow Water Equivalence downloader.
+
+        :param username: NASA Earthdata username
+        :param password: NASA Earthdata password
+        :param kwargs: keys to use as default in url_template
+        '''
+
+        ## Get formatting keys of url_template
+        self.url_keys = [k[1] for k in Formatter().parse(self.url_template)]
+
+        ## Function to format URL
+        self.format_url = partial(self.url_template.format)
+
+        ## Set url template defaults to specified defaults
+        self.set_defaults(**self.defaults) ## global defaults (in this class)
+        self.set_defaults(**kwargs)        ## instance defaults
+
+        ## Auth Detauls
+        self.username = username
+        self.password = password
+
+        ## Tet up session
+        self.session = requests.session()
+        self.get_auth()
+
+        ## Output
+        self.folder = folder
+
+    def set_defaults(self, **kwargs):
+        '''
+        Set defaults for url template
+        '''
+
+        for key in kwargs:
+            if key in self.url_keys:
+                self.format_url.keywords[key] = kwargs[key]
+
+        return(self.format_url.keywords)
+
+    def get_auth(self):
+        '''
+        Get download authentication
+
+        How authentication works:
+        (1) request to some sort of NSIDC URL
+        (2) get HTTP 302 response redirecting to urs.earthdata.nasa.gov OAuth
+        (3) login at urs.earthdata.nasa.gov oauth
+        (4) use oauth tokens from urs.earthdata.nasa.gov to request any download url
+        '''
+
+        test_url = "{protocol}://{server}/SMAP/".format(**self.format_url.keywords)
+
+        ## User / pass to use with auth.
+        ## Will attempt to use auth in ~/.netrc if not passed
+        if self.username and self.password:
+            self.session.auth = (self.username, self.password)
+
+        ## Send test request, keep following the redirects and scoop
+        ## up all of the cookies along the way in self.session
+        try:
+            req = self.session.get(test_url, allow_redirects = False)
+
+            while req.status_code == 302:
+                req = self.session.get(req.headers["Location"],
+                                       allow_redirects = False)
+        except requests.ConnectionError:
+            pass
+
+        ## If the final request is 401 (Bad Auth), throw exception
+        if req.status_code == 401:
+            raise PermissionError("Bad NASA Earthdata Authentication!")
+
+    def download_file(self, folder=None, overwrite=False, **kwargs):
+        '''
+        Download a file of particular kwargs
+        '''
+
+        url = self.format_url(**kwargs)
+
+        ## Dict of all the keywords:vals going into URL
+        all_keywords = {**kwargs, **self.format_url.keywords}
+
+        ## Format output dir with keywords if there's none passed
+        if not folder:
+            folder = self.folder.format(all_keywords)
+
+        ## Prepare file system
+        filename = url.split("/")[-1]
+        filepath = "{}/{}".format(folder, filename)
+        block_size = 1024
+        #print("{}".format(filename))
+
+        if os.path.exists(filepath):
+            if overwrite:
+                os.remove(filepath)
+            else:
+                print(" ** (skipping...) **")
+                return filename
+
+        ## Download the dang thing
+        with self.session.get(url, stream=True) as r:
+
+            if r.status_code == 404:
+                raise FileNotFoundError("File Not Found: {}".format(url))
+
+            ## Open file
+            with open(filepath, 'wb') as f:
+
+                pbar = tqdm(initial=0,
+                            total=int(r.headers['content-length']),
+                            unit='B',
+                            unit_scale=True)
+
+                ## Create dest folder if not exist
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+
+                ## Stream content to file in chunks
+                for chunk in r.iter_content(block_size):
+                    f.write(chunk)
+                    pbar.update(block_size)
+
+            pbar.close()
+
+        return filename  ## changed to filename from filepath to fix another script
+
+    def download_range(self, date, **kwargs):
+        '''
+        Download a range of dates for a particular sensor
+        '''
+
+        assert isinstance(date, list), "Range passed must be list of dates"
+
+        for date in pd.date_range(date[0], date[1]):
+            kwargs["date"] = date
+            self.download_file(**kwargs)
+
+dl = nsidcDownloader(username="dale0007",
+                     password="SnowWater1")
+
+dataset = {
+    "resolution": "3.125km",
+    "platform": "F17",
+    "sensor": "SSMIS",
+    "date": datetime(2015,10,10),
+    "channel": "37H"
+}
---
+++ build/lib/swepy/swepy.py
@@ -0,0 +1,247 @@
+import datetime
+
+import requests
+from nsidcDownloader import nsidcDownloader
+import numpy as np
+from netCDF4 import Dataset
+from skimage.measure import block_reduce
+
+from mapboxgl.utils import *
+from mapboxgl.viz import *
+
+import pandas as pd
+import ease2conv as e2
+from nco import Nco
+import os
+from tqdm import tqdm
+import glob
+nco = Nco()
+
+class swepy():
+    '''Class Members'''
+    def __init__(self, working_dir, start, end, ul, lr,
+                outfile19 = 'all_days_19H.nc', outfile37 = 'all_days_37H.nc'):
+        '''User instantiates the class with working directory,
+        date ranges, and lat/lon bounding coords. constructor gets
+        the datetime list, x/y coords, and file directories'''
+        self.working_dir = working_dir
+        self.path19, self.path37, self.wget = self.get_directories(working_dir)
+
+        self.outfile_19 = outfile19
+        self.outfile_37 = outfile37
+
+        self.dates = pd.date_range(start, end)
+        self.geo_list = self.get_xy(ul, lr)
+
+        self.down19list = []
+        self.down37list = []
+        self.sub19list = []
+        self.sub37list = []
+
+
+    def get_directories(self, path):
+        os.chdir(path)
+        wget = path + "/data/wget/"
+        path19 = path + "/data/Subsetted_19H/"
+        path37 = path + "/data/Subsetted_37H/"
+
+        if not os.path.exists(wget):
+            os.makedirs(wget)
+        if not os.path.exists(path19):
+            os.makedirs(path19)
+        if not os.path.exists(path37):
+            os.makedirs(path37)
+        return path19, path37, wget
+
+
+    def get_xy(self, ll_ul, ll_lr):
+        '''Use NSIDC scripts to convert user inputted
+        lat/lon into Ease grid 2.0 coordinates'''
+        N3 = e2.Ease2Transform("EASE2_N3.125km")
+        # get x,y for 3.125
+        row, col = N3.geographic_to_grid(ll_ul[0], ll_ul[1])
+        x3ul, y3ul = N3.grid_to_map(row, col)
+        row, col = N3.geographic_to_grid(ll_lr[0], ll_lr[1])
+        x3lr, y3lr = N3.grid_to_map(row, col)
+        list_3 = [x3ul, y3ul, x3lr, y3lr]
+        return list_3
+
+
+    def subset(self, scrape = False):
+        '''pass geo-coord list and directory path
+        script will get the files from wget directory
+        and subset them geographically'''
+        os.chdir(self.wget)
+        for file in tqdm(self.down19list):
+            outfile = self.path19 + file
+            infile = self.wget + file
+            opt = [
+                "-d x,%f,%f" % (self.geo_list[0],self.geo_list[2]),
+                "-d y,%f,%f" % (self.geo_list[3],self.geo_list[1]),
+                "-v TB"
+            ]
+            nco.ncks(input=infile, output=outfile, options=opt)
+            self.sub19list.append(outfile)
+            os.remove(infile)
+
+        for file in tqdm(self.down37list):
+            outfile = self.path37 + file
+            infile = self.wget + file
+            opt = [
+                "-d x,%f,%f" % (self.geo_list[0],self.geo_list[2]),
+                "-d y,%f,%f" % (self.geo_list[3],self.geo_list[1]),
+                "-v TB"
+            ]
+            nco.ncks(input=infile, output=outfile, options=opt)
+            self.sub37list.append(outfile)
+            os.remove(infile)
+        # Empty the download lists
+        self.down19list = []
+        self.down37list = []
+        return
+
+    def scrape_all(self):
+        '''Function to ensure we subset
+         and concatenate every year!
+         Implements the whole workflow!'''
+        nD = nsidcDownloader(folder = self.wget)     #instantiate downloading class
+        outfile19 = 'all_days_19H.nc'
+        outfile37 = 'all_days_37H.nc'
+        if len(self.dates) <= 133:
+            for date in self.dates:
+                file19 = self.get_file(date, "19H")
+                file37 = self.get_file(date, "37H")
+                self.down19list.append(nD.download_file(**file19))
+                self.down37list.append(nD.download_file(**file37))
+            self.subset()
+            return self.concatenate()
+        else:
+            comp_list = [self.dates[x:x + 100] for x in range(0, len(self.dates), 100)]
+            for count, subList in enumerate(comp_list):
+                tempfile19 = '19H' + str(count) + 'temp.nc'
+                tempfile37 = '37H' + str(count) + 'temp.nc'
+                for date in subList:
+                    file19 = get_file(date, "19H")
+                    file37 = get_file(date, "37H")
+                    self.down19list.append(nD.download_file(**file19))
+                    self.down37list.append(nD.download_file(**file37))
+                self.subset()
+                #concatenate(self, tempfile19, tempfile37)
+            return self.concatenate()
+
+
+    def get_file(self, date, channel): # add more defaulting params
+        '''Function that uses date and channel to
+        find optimal file composition and return the
+        file params for the web scraper's use.'''
+        sensors = {1992: 'F11', 1993: 'F11', 1994: 'F11', 1995: 'F11', 1996: 'F13', 1997: 'F13', 1998: 'F13',
+                1999: 'F13', 2000: 'F13', 2001: 'F13', 2002: 'F13', 2003: 'F15', 2004: 'F15', 2005: 'F15', 2006: 'F15',
+                2007: 'F15', 2008: 'F16', 2009: 'F17', 2010: 'F17', 2011: 'F17', 2012: 'F17', 2013: 'F17', 2014: 'F18',
+                2015: 'F19'}
+        ssmi_s = "SSMIS" if sensors[date.year] in ['F16', 'F17', 'F18', 'F19'] else "SSMI"
+        resolution = '6.25km' if channel == '19H' else '3.125km'
+        file = {
+            "resolution": resolution,
+            "platform": sensors[date.year],
+            "sensor": ssmi_s,
+            "date": date,
+            "channel": channel,
+            "dataversion": 'v1.3' if date.year == 2015 else 'v1.2'
+        }
+        return file
+
+    ## use list of paths as parameter to concatenate all paths in list
+    ## ['/folder/file1.nc','/folder/file2.nc']
+    def concatenate(self, subset = False):
+        '''Function to concatenate files in the subsetted data
+        folders. Takes working directory path, the desired outfile
+        names, and whether or not this is a final pass in the subet_all
+        function as input.'''
+        os.chdir(self.path19)
+        # Concatenate 19GHz files:
+        if len(self.sub19list) != 0:
+            nco.ncrcat(input=self.sub19list, output = self.outfile_19, options=["-O"])
+        else:
+            print("No 19Ghz Files to Concatenate")
+        # Concatenate 37GHz files:
+        os.chdir(self.path37) # do i want to do this now? could put them somewhere else?
+        if len(self.sub37list) != 0:
+            nco.ncrcat(input = self.sub37list, output = self.outfile_37, options = ["-O"])
+        else:
+            print("No 37Ghz Files to Concatenate")
+
+        self.clear_sub_files() # clean out files that were concat
+
+        return self.outfile_19, self.outfile_37
+
+
+    def clear_sub_files(self):
+        os.chdir(self.path19)
+        filelist = glob.glob('NSIDC*')
+        for f in filelist:
+            os.remove(f)
+        os.chdir(self.path37)
+        filelist = glob.glob('NSIDC*')
+        for f in filelist:
+            os.remove(f)
+        return
+
+
+    def scrape(self):
+        '''Wrapper function to allow more selective use of just the
+            web scraper'''
+        nD = nsidcDownloader(folder = self.wget)
+        for date in self.dates:
+            file19 = self.get_file(date, "19H")
+            file37 = self.get_file(date, "37H")
+            self.down19list.append(nD.download_file(**file19))
+            self.down37list.append(nD.download_file(**file37))
+
+        return
+
+
+
+
+
+def plot_a_day(file1, file2, path, token):
+    '''read tb,x,y data from final files,
+    with the purpose of plotting.'''
+    os.chdir(path + '/data/Subsetted_19H')
+    fid_19H = Dataset(file1, "r", format="NETCDF4")
+    os.chdir(path + '/data/Subsetted_37H')
+    fid_37H = Dataset(file2, "r", format="NETCDF4")
+    x = fid_19H.variables['x'][:]
+    y = fid_37H.variables['y'][:]
+    tb_19H = fid_19H.variables['TB'][:]
+    tb_37H = fid_37H.variables['TB'][:]
+    tb_37H = block_reduce(tb_37H, block_size = (1,2,2), func = np.mean)
+    tb= tb_19H  - tb_37H
+    lats = np.zeros((len(y), len(x)), dtype=np.float64)
+    lons = np.zeros((len(y), len(x)), dtype=np.float64)
+    grid = e2.Ease2Transform(gridname=fid_19H.variables["crs"].long_name)
+    for i, xi in enumerate(x):
+        for j, yj in enumerate(y):
+            row, col = grid.map_to_grid(xi, yj)
+            lat, lon = grid.grid_to_geographic(row, col)
+            lats[j, i] = lat
+            lons[j, i] = lon
+    one_day = tb[0,:,:]
+    df = pd.DataFrame(columns = ['lat', 'lon', 'swe'])
+    for i in range(len(one_day[:,1])):
+        for j in range(len(one_day[1,:])):
+            df = df.append({'lat': lats[i][j], 'lon': lons[i][j], 'swe':one_day[i][j]}, ignore_index = True)
+    df_to_geojson(df, filename = 'swe_1day.geojson',properties = ['swe'],lat = 'lat', lon = 'lon')
+    measure = 'swe'
+    color_breaks = [round(df[measure].quantile(q=x*0.1), 2) for x in range(1,9)]
+    color_stops = create_color_stops(color_breaks, colors='YlGnBu')
+
+    # Create the viz from the dataframe
+    viz = CircleViz('swe_1day.geojson',
+                    access_token=token,
+                    color_property = "swe",
+                    color_stops = color_stops,
+                    center = (-154, 67),
+                    zoom = 3,
+                    below_layer = 'waterway-label')
+
+    viz.show()
