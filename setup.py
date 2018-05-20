d35787a948f99ce8cf47c3803903ce2c7eadd501
WILLIAM Norris <williamnorris@WILLIAMs-MacBook-Pro.local>
2018-05-20 09:27:23 -0600

    restructuring git for pip library compatability

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='swepy',
+      version='0.1',
+      description='The easiest way to work with Tb data',
+      url='https://github.com/wino6687/SWEpy',
+      author='William Norris',
+      author_email='wino6687@colorado.edu',
+      license='CU',
+      packages=['swepy'],
+      zip_safe=False)
