6b03b4aad6a4f0470c9ddb644b203ecca8329eef
WILLIAM Norris <williamnorris@WILLIAMs-MacBook-Pro.local>
2018-05-20 14:03:27 -0600

    adding everything for pip installation

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='swepy',
-      version='0.1',
-      description='The easiest way to work with Tb data',
-      url='https://github.com/wino6687/SWEpy',
-      author='William Norris',
-      author_email='wino6687@colorado.edu',
-      license='CU',
-      packages=['swepy'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swepy",
+    version="0.0.1",
+    author="William Norris",
+    author_email="wino6687@colorado.edu",
+    description="A python package for obtaining and cleaning Tb files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wino6687/SWEpy",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
