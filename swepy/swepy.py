e3f588b1cca346f02fd5ec40b8939d104ca4db51
WILLIAM Norris <williamnorris@cu-biot-3-10.203.137.235.int.colorado.edu>
2018-05-21 13:51:17 -0600

    v 0.0.5, I think I have fixed the missing downloader module. pushing and uploading to pypi to test

--- swepy/swepy.py
+++ swepy/swepy.py
@@ -1,8 +1,7 @@
 import datetime
 
 import requests
-import nsidcDownloader
-from nsidcDownloader import nsidcDownloader
+from swepy.nsidcDownloader import nsidcDownloader
 import numpy as np
 from netCDF4 import Dataset
 from skimage.measure import block_reduce
