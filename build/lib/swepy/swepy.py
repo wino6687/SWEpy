976f483b23fb65ada2a6cbf90858819a52bc540f
wino6687 <wino6687@colorado.edu>
2019-02-06 08:55:27 -0700

    updated distribution files for travis

--- build/lib/swepy/swepy.py
+++ build/lib/swepy/swepy.py
@@ -151,6 +151,7 @@ class swepy():
         else:
             print("No usable bounding coordinates or grid given, \
              please specify bounds or a grid to scrape.")
+        return 
 
 
     def get_grid(self, lat1, lat2):
@@ -193,10 +194,10 @@ class swepy():
             working directory to create data directories
         '''
         os.chdir(path)
-        paths = ["/data/wget/", "/data/Subsetted_19H/", "/data/Subsetted_37H/"]
-        for path in paths:
-            if not os.path.exists(path):
-                os.makedirs(path)
+        paths = ["./data/wget/", "./data/Subsetted_19H/", "./data/Subsetted_37H/"]
+        for path1 in paths:
+            if not os.path.exists(path1):
+                os.makedirs(path1)
         return paths[0], paths[1], paths[2]
 
 
@@ -473,8 +474,9 @@ class swepy():
     def safe_subtract(self, tb19, tb37):
         '''
         Check size of each file, often the 19 and 37
-        files are one unit off of eachother. This will
-        chop the larger one to match the smaller one.
+        matrices are one unit off of eachother.
+
+        Chops the larger matrix to match the smaller matrix
         '''
         shape1 = np.shape(tb19)
         shape2 = np.shape(tb37)
@@ -517,6 +519,15 @@ class swepy():
         '''
         Open 19 and 37 files, block reduce the 37ghz to match,
         then safe subtract the two to get SWE, tb19, and tb37 returned
+
+        Parameters: 
+        -----------
+        tb19: str
+            netCDF file containing 19GHz imagery 
+        tb37: str
+            netCDF file containing 37Ghz imagery 
+        high: boolean 
+            high or low resolution imagery (default: True)
         '''
         filename_19H = tb19
         fid_19H = Dataset(filename_19H, "r", format = "NETCDF4")
