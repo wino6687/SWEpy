e9b6ca1069ecd911f9ef7c6ebf4209255b87a9b2
wino6687 <wino6687@colorado.edu>
2019-03-27 11:49:56 -0600

    cleaning up processing script May remove some of the yearly analysis stuff, or stick it in a seperate module Either way need to spend more time thinking about what i want the processing module to be and how extensive it should be

---
+++ swepy/process/process.py
@@ -0,0 +1,181 @@
+import datetime
+import time
+import os
+import warnings 
+import numpy as np
+from numpy import newaxis 
+from numpy import shape
+import pandas as pd 
+from netCDF4 import Dataset
+from skimage.measure import block_reduce
+import math
+from swepy.swepy import swepy
+from scipy.signal import savgol_filter
+from jenks import jenks
+from scipy.cluster.vq import *
+
+
+class Process():
+    """
+    Facilitate the cleaning, and processing of tb files for SWE analysis 
+    """
+    def __init__(self, tb19, tb37):
+        self.tb19 = tb19
+        self.tb37 = tb37
+
+    
+    def get_array(self, file19, file37, high = True):
+        """
+        Take 19H and 37H netCDF files, open and store tb 
+        data in np arrays
+
+        Parameters: 
+        -----------
+        file19: str
+            filename for 19H file 
+        file37: str
+            filename for 37H file 
+        high: bool
+            true = high resolution imagery (3.125km/6.25km)
+            false = low resolution imagery (25km)
+        """
+        fid_19H = Dataset(file19,"r", format = "NETCDF4")
+        fid_37H = Dataset(file37, "r", format = "NETCDF4")
+        tb_19H =fid_19H.variables["TB"][:]
+        tb_37H = fid_37H.variables["TB"][:]
+        if high:
+            tb_37H = block_reduce(tb_37H, block_size = (1,2,2), func = np.mean)
+        return tb_19H, tb_37H
+
+    def get_count(self, classes):
+        """
+        Given class  boundaries, return a dict of counts within 
+        each class 
+
+        Parameters: 
+        -----------
+        classes: list 
+            list of class boundaries to compute counts on
+        """
+        unique, counts = np.unique(classes, return_counts=True)
+        return dict(zip(unique, counts))
+
+    def pandas_fill(self, arr):
+        """
+        --INEFFICIENT--
+        Given 2d array, convert to pd dataframe
+        and ffill missing values in place
+
+        Parameters: 
+        -----------
+        arr: np.array 
+            Ideally time vector of swe cube
+        """
+        df = pd.DataFrame(arr)
+        df.fillna(method='ffill', inplace = True)
+        out = df.values
+        return out
+
+    def get_df(self, df,year):
+        """
+        --Yearly Analysis--
+        Given full time series df, subset and return only 
+        the data from a given year
+
+        Parameters: 
+        df: pd.DataFrame
+            full time series to subset 
+        year: int
+            year to subset dataframe on 
+        """
+        mask_year = df['time'].dt.year == year
+        new_df = df[mask_year]
+        counts = new_df['count'].values
+        return counts
+
+    def goodness_of_variance_fit(self, array, classes):
+        """
+        --Jenks Classification--
+        Given an array and a number of classes, determine gvf
+        Aids in determining optimal jenks breaks for classification of SWE
+
+        Parameters: 
+        ----------
+        
+        """
+        # get the break points
+        classes = jenks(array, classes)
+        # do the actual classification
+        classified = np.array([classify(i, classes) for i in array])
+        # max value of zones
+        maxz = max(classified)
+        # nested list of zone indices
+        zone_indices = [[idx for idx, val in enumerate(classified) if zone + 1 == val] for zone in range(maxz)]
+        # sum of squared deviations from array mean
+        sdam = np.sum((array - array.mean()) ** 2)
+        # sorted polygon stats
+        array_sort = [np.array([array[index] for index in zone]) for zone in zone_indices]
+        # sum of squared deviations of class means
+        sdcm = sum([np.sum((classified - classified.mean()) ** 2) for classified in array_sort])
+        # goodness of variance fit
+        gvf = (sdam - sdcm) / sdam
+        return gvf
+
+    def classify(self,value, breaks):
+        """
+        --Jenks Classification--
+        classify a value into a given bin 
+
+        Parameters: 
+        ----------
+        value: float
+        breaks: list 
+        """
+        for i in range(1, len(breaks)):
+            if value < breaks[i]:
+                return i
+        return len(breaks) - 1
+
+    def vector_clean(self, cube):
+        """
+        --Data Processing--
+        Clean erroneous spikes out of 37Ghz cube
+
+        Parameters: 
+        cube: np.array(t,x,y)
+            np array time cube of 37GHz tb data 
+            Note: can be used with other arrays, 
+            but is looking for patterns in 37H files
+        """
+        cube[cube  == 0] = np.nan
+        for i in range(np.shape(cube)[0]):
+            arr = cube[i,:,:]
+            mask = np.isnan(arr)
+            idx = np.where(~mask,np.arange(mask.shape[1]),0)
+            np.maximum.accumulate(idx,axis=1, out=idx)
+            cube[i,:,:] = arr[np.arange(idx.shape[0])[:,None], idx]
+        return cube
+
+    def apply_filter(self, cube): 
+        '''
+        --Data Processing--
+        Apply a sav-gol filter from scipy to time vector's of cube
+        Can be used with either tb files or differences SWE values 
+
+        Parameters: 
+        ----------
+        cube: np.array(t,x,y)
+            np array time cube of swe for passive microwave data 
+        '''
+        shape = np.shape(cube)
+        smooth_cube = np.empty((shape[0],shape[1],shape[2]))
+        for x in range(shape[1]):
+            for y in range(shape[2]):
+                pixel_drill = cube[:,x,y]
+                #forward fill data 
+                #pixel = pixel_drill[~np.isnan(pixel_drill)]
+                pixel = pandas_fill(pixel_drill)
+                yhat = savgol_filter(np.squeeze(pixel), 55, 5) # window size 51, polynomial order 3
+                yhat[yhat<3] = 0
+                smooth_cube[:,x,y] = yhat
+        return smooth_cube
