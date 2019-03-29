fixed test functions adding fixtures to simplify repeated computation in testing

swepy/tests/test_swepy.py
@@ -159,7 +159,7 @@ def test_scrape():
     s1 = swepy(os.getcwd()+'/swepy/tests/',start = date, end = date, ul = 'N', lr = 'N', username = 'test', password = 'test')
     s1.scrape()
     list1 = glob.glob("*.nc")
-    assert list1 == ['NSIDC-0630-EASE2_N3.125km-F17_SSMIS-2010001-37H-M-SIR-CSU-v1.3.nc','NSIDC-0630-EASE2_N6.25km-F17_SSMIS-2010001-19H-M-SIR-CSU-v1.3.nc']
+    assert list1 == ['NSIDC-0630-EASE2_N6.25km-F17_SSMIS-2010001-19H-M-SIR-CSU-v1.3.nc','NSIDC-0630-EASE2_N3.125km-F17_SSMIS-2010001-37H-M-SIR-CSU-v1.3.nc']
 
 def test_subset():
     if not os.path.exists('sub'):
@@ -188,16 +188,16 @@ def test_get_directories():
     s1 = swepy(os.getcwd())
     assert os.path.exists(os.getcwd()+'/data') == True
 
-
 @pytest.fixture
 def scraped_files():
     date = datetime.date(2010,1,1)
     ul = 'N'
     lr = 'N'
-    s1 = swepy(os.getcwd()+'/swepy/tests/',start = date, end = date, ul = 'N', lr = 'N', username = 'test', password = 'test')
+    s1 = swepy(os.getcwd(),start = date, end = date, ul = 'N', lr = 'N', username = 'test', password = 'test')
     files = s1.scrape()
-    return files
+    return (files[0][0], files[1][0])
+
 
 def test_get_array(scraped_files):
     tb19, tb37 = process.get_array(scraped_files[0], scraped_files[1])
-    assert type(tb19) == numpy.ma.core.MaskedArray
+    assert type(tb19) == np.ma.core.MaskedArray