e770d707ab56a55a05da7d18d166f5f476f6debf
wino6687 <wino6687@colorado.edu>
2019-03-29 12:41:58 -0600

    removed jenks classification that is really more of an analysis tool than a processing tool

--- swepy/process/process.py
+++ swepy/process/process.py
@@ -11,7 +11,7 @@ from skimage.measure import block_reduce
 import math
 from swepy.swepy import swepy
 from scipy.signal import savgol_filter
-from jenks import jenks
+# from jenks import jenks
 from scipy.cluster.vq import *
 
 
@@ -95,48 +95,6 @@ class Process():
         counts = new_df['count'].values
         return counts
 
-    def goodness_of_variance_fit(self, array, classes):
-        """
-        --Jenks Classification--
-        Given an array and a number of classes, determine gvf
-        Aids in determining optimal jenks breaks for classification of SWE
-
-        Parameters: 
-        ----------
-        
-        """
-        # get the break points
-        classes = jenks(array, classes)
-        # do the actual classification
-        classified = np.array([classify(i, classes) for i in array])
-        # max value of zones
-        maxz = max(classified)
-        # nested list of zone indices
-        zone_indices = [[idx for idx, val in enumerate(classified) if zone + 1 == val] for zone in range(maxz)]
-        # sum of squared deviations from array mean
-        sdam = np.sum((array - array.mean()) ** 2)
-        # sorted polygon stats
-        array_sort = [np.array([array[index] for index in zone]) for zone in zone_indices]
-        # sum of squared deviations of class means
-        sdcm = sum([np.sum((classified - classified.mean()) ** 2) for classified in array_sort])
-        # goodness of variance fit
-        gvf = (sdam - sdcm) / sdam
-        return gvf
-
-    def classify(self,value, breaks):
-        """
-        --Jenks Classification--
-        classify a value into a given bin 
-
-        Parameters: 
-        ----------
-        value: float
-        breaks: list 
-        """
-        for i in range(1, len(breaks)):
-            if value < breaks[i]:
-                return i
-        return len(breaks) - 1
 
     def vector_clean(self, cube):
         """
