3497d2b2414ed915034325b77b9dfb51dd3703de
wino6687 <wino6687@colorado.edu>
2019-04-09 13:59:29 -0600

    added more downloader unit tests and broke out some tests from test_swepy

--- swepy/tests/test_swepy.py
+++ swepy/tests/test_swepy.py
@@ -180,31 +180,6 @@ def test_safe_subtract2():
         assert np.shape(tb) == (1,151,151)
 
 
-def test_nD_local():
-    date = datetime.date(2010,1,1)
-    file = {
-        "protocol": "http",
-        "server": "localhost:8000",
-        "datapool": "MEASURES",
-        "dataset": "NSIDC-0630",
-        "version": "001",
-        "projection": "EASE2",
-        "resolution": "6.25km",
-        "platform":"F17",
-        "sensor": "SSMIS",
-        'date1': datetime.date(2010, 1, 1),
-        'date2': datetime.date(2010, 1, 1),
-        "channel": '19H',
-        "grid": "N",
-        "pass": "M",
-        "algorithm": "SIR",
-        "input": "CSU",
-        "dataversion": "v1.3"
-    }
-    nD = nsidcDownloader.nsidcDownloader(no_auth = True)
-    nD.download_file(**file)
-    list1 = glob.glob('*.nc')
-    assert 'NSIDC-0630-EASE2_N6.25km-F17_SSMIS-2010001-19H-M-SIR-CSU-v1.3.nc' in list1
 
 def test_scrape_fail():
     date = datetime.date(2010,1,1)
@@ -251,10 +226,6 @@ def test_get_directories():
     s1 = swepy(os.getcwd())
     assert os.path.exists(os.getcwd()+'/data') == True
 
-
-def test_get_auth():
-    with pytest.raises(PermissionError):
-        nD = nsidcDownloader.nsidcDownloader('room', 'backend')
 
 
 def test_grid_to_geo():
