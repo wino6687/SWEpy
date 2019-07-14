eff6264bd53b42920c62ff44e570b4f8b7c749e0
wino6687 <wino6687@colorado.edu>
2019-07-14 17:36:57 -0600

    converting back to conda for everything adding the mock server spin up;

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,7 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  source venv/bin/activate
+  source $HOME/miniconda/bin/activate
+  export PATH="$HOME/miniconda/bin:$PATH"
+  conda activate swepy_env
   python -m pytest -v
 else
   source $HOME/miniconda/bin/activate
