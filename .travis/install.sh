e98b6887762c2c5148cecb04c0b2871fefd18845
wino6687 <wino6687@colorado.edu>
2019-07-14 17:45:41 -0600

    linux is passing, osx is failing, trying to fix this

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,8 @@ if [$TRAVIS_OS_NAME = 'osx']; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
     bash miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
-    echo "conda activate base" >> ~/.bashrc
-    source $HOME/miniconda/bin/activate
+    # echo "conda activate base" >> ~/.bashrc
+    # source $HOME/miniconda/bin/activate
     conda config --set always_yes yes --set show_channel_urls true --set changeps1 no
     conda update -q conda
     conda config --add channels conda-forge
