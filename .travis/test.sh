b021b07975769f3515afde83fc7d8584588392f2
wino6687 <wino6687@colorado.edu>
2019-07-14 17:14:32 -0600

    fixed testing on wrong os error

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,8 +1,8 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  source $HOME/miniconda/bin/activate
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda activate swepy_env
+  source activate venv
   python -m pytest -v
 else
+  source $HOME/miniconda/bin/activate
+  export PATH="$HOME/miniconda/bin:$PATH"
   python -m pytest -v
 fi
