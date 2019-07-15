8a70172ec8f21c9c87b41c65c41b8bd4029491d5
wino6687 <wino6687@colorado.edu>
2019-07-15 12:26:26 -0600

    fixing missing command in osx testing suite

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,10 +4,10 @@ if [$TRAVIS_OS_NAME = 'osx']; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
     bash miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
-    # echo "conda activate base" >> ~/.bashrc
+    echo "conda activate base" >> ~/.bashrc
     # source $HOME/miniconda/bin/activate
     conda config --set always_yes yes --set show_channel_urls true --set changeps1 no
-    conda update -q conda
+    conda update conda
     conda config --add channels conda-forge
     conda info -a
     conda init bash
