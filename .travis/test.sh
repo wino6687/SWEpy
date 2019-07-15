ca936cf53de671849e9adef3458d40e95f4bd00e
wino6687 <wino6687@colorado.edu>
2019-07-15 13:37:53 -0600

    switching to conda activate in osx

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,7 +1,7 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   source $HOME/miniconda/bin/activate
   export PATH="$HOME/miniconda/bin:$PATH"
-  source activate swepy_env
+  conda activate swepy_env
   python -m pytest -v
 else
   source $HOME/miniconda/bin/activate
