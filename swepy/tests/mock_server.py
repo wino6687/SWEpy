7ffc9a2db11e69e827e495bfff3725ebece4af01
wino6687 <wino6687@colorado.edu>
2019-07-22 12:50:58 -0600

    black

--- swepy/tests/mock_server.py
+++ swepy/tests/mock_server.py
@@ -4,6 +4,7 @@ from http.server import HTTPServer as BaseHTTPServer, SimpleHTTPRequestHandler
 
 class HTTPHandler(SimpleHTTPRequestHandler):
     """This handler uses server.base_path instead of always using os.getcwd()"""
+
     def translate_path(self, path):
         path = SimpleHTTPRequestHandler.translate_path(self, path)
         relpath = os.path.relpath(path, os.getcwd())
@@ -13,13 +14,16 @@ class HTTPHandler(SimpleHTTPRequestHandler):
 
 class HTTPServer(BaseHTTPServer):
     """The main server, you pass in base_path which is the path you want to serve requests from"""
-    def __init__(self, base_path, server_address, RequestHandlerClass=HTTPHandler):
+
+    def __init__(
+        self, base_path, server_address, RequestHandlerClass=HTTPHandler
+    ):
         self.base_path = base_path
         BaseHTTPServer.__init__(self, server_address, RequestHandlerClass)
 
 
-web_dir = os.path.join(os.path.dirname(__file__), 'data')
+web_dir = os.path.join(os.path.dirname(__file__), "data")
 httpd = HTTPServer(web_dir, ("", 8000))
 httpd.serve_forever()
 
-# serves files with pathway localhost:8000/file_name
+# serves files on localhost:8000/file_name
