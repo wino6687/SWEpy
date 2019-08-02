371cbf28e4a13338e208744561244b952c5dadb2
wino6687 <wino6687@colorado.edu>
2019-08-02 14:37:10 -0600

    removing if/else in test.sh since actions are the same on each end!

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,11 +1,5 @@
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  source $HOME/miniconda/bin/activate
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda activate swepy_env
-  python -m pytest -v --cov=swepy
-else
-  source $HOME/miniconda/bin/activate
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda activate swepy_env
-  python -m pytest -v --cov=swepy
-fi
+
+source $HOME/miniconda/bin/activate
+export PATH="$HOME/miniconda/bin:$PATH"
+conda activate swepy_env
+python -m pytest -v --cov=swepy
