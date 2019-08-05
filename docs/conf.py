33017bd9249545811ba0afa40b3e1f11e7c1830f
wino6687 <wino6687@colorado.edu>
2019-08-05 10:15:01 -0600

    changing v numbers too

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "will norris"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.4.2"
+release = "1.4.0"
 
 
 # -- General configuration ---------------------------------------------------
