124e8af32075f22aaabbc5cccb13f770863a6ce2
wino6687 <wino6687@colorado.edu>
2019-09-11 10:12:19 -0600

    Bump version: 1.8.1 → 1.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swepy",
-    version="1.8.1",
+    version="1.8.2",
     author="William Norris",
     author_email="wino6687@colorado.edu",
     description="A python package for obtaining and manipulating Tb files from the MEaSUREs database",
